cc21723648227249e3cf6648b42795341e8cdf2b
Mitko Tochev <mr.sir88@gmail.com>
2018-11-01 19:20:11 -0400

    about  section of home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,5 +6,25 @@
 @endsection
 
 @section('content')
-   Content goes here
+   <div class="main-grid">
+       <div class="about-grid">
+           <div class="title">ABOUT VANE CARNERO</div>
+           <div class="sub-title">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit eros dictum ante pharetra maximus. Suspendisse sapien magna, tincidunt convallis semper at, imperdiet sit amet risus. Duis in eros massa.
+           </div>
+           <div class="description">
+               Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est. Donec efficitur nibh id ullamcorper molestie. Nam aliquet, risus et finibus imperdiet, mi est faucibus sapien, at molestie dolor neque eu neque. Suspendisse vitae neque in tellus tincidunt imperdiet sed a justo. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+           </div>
+           <div class="book">
+               <img src="/img/book-front.jpg"/>
+           </div>
+           <div class="book-button">
+               <button class="button" onclick="window.load('/book');">Learn more &gt;</button>
+           </div>
+       </div>
+       <div class="what-we-do-grid">What we do</div>
+       <div class="testimonials-grid">testimonials</div>
+       <div class="blog-feed-grid">blog feed </div>
+       <div class="work-with-us-grid">work with us</div>
+   </div>
 @endsection
