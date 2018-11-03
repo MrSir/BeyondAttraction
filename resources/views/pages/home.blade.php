f7ff1296a9872e54976e6153f29a067db6188d93
Mitko Tochev <mr.sir88@gmail.com>
2018-11-02 20:04:54 -0400

    implemented the rest of the mobile home page skeleton

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -31,19 +31,19 @@
                Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est. Donec efficitur nibh id ullamcorper molestie.
            </div>
            <div class="poduct-1 product-grid">
-               <div class="title">Product 1</div>
+               <div class="title">Product 1: Get Started</div>
                <div class="description">
                    Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est.
                </div>
            </div>
            <div class="poduct-2 product-grid">
-               <div class="title">Product 2</div>
+               <div class="title">Product 2: Advanced</div>
                <div class="description">
                    Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est.
                </div>
            </div>
            <div class="poduct-3 product-grid">
-               <div class="title">Product 3</div>
+               <div class="title">Product 3: Ultimate</div>
                <div class="description">
                    Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est.
                </div>
@@ -59,23 +59,25 @@
            </div>
        </div>
        <div class="blog-feed-grid">
-           @if(count($articles) === 0)
-               <div>
-                   No articles found.
-               </div>
-           @endif
            @foreach($articles as $article)
                <div class="post-grid">
-                   <img src="{{ $article->cover->link }}"/>
-                   <div class="title"><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></div>
-                   <div class="date">Date: {{ $article->published_at->format('Y-m-d') }}</div>
+                   <a href="/blog/{{ $article->id }}"><img src="{{ $article->cover->link }}"/></a>
+                   <div class="title"><a href="/blog/{{ $article->id }}">{{ $article->title }}</a></div>
                    <div class="summary">
                        {{ $article->summary }}<br/>
-                       <a href="/articles/{{ $article->id }}">Read More</a>
+                       <a href="/blog/{{ $article->id }}">Read More</a>
                    </div>
                </div>
            @endforeach
        </div>
-       <div class="work-with-us-grid">work with us</div>
+       <div class="work-with-us-grid">
+           <div class="title">WORK WITH US</div>
+           <div class="description">
+               Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est. Donec efficitur nibh id ullamcorper molestie. Nam aliquet, risus et finibus imperdiet, mi est faucibus sapien, at molestie dolor neque eu neque. Suspendisse vitae neque in tellus tincidunt imperdiet sed a justo. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+           </div>
+           <div class="get-started-button">
+               <button class="button" onclick="window.load('/book');">Get Started Now &gt;</button>
+           </div>
+       </div>
    </div>
 @endsection
