69eb35fcc0ec5a2a8d47d95f6eb1e91248a436ec
Mitko Tochev <mr.sir88@gmail.com>
2018-11-04 19:01:40 -0500

    styled the rest of the home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('page-header')
     <link href="/css/pages/home-style.css" rel="stylesheet">
-    {{--<link href="/css/pages/home-large-style.css" rel="stylesheet">--}}
+    <link href="/css/pages/home-large-style.css" rel="stylesheet">
 @endsection
 
 @section('content')
@@ -30,19 +30,19 @@
            <div class="description">
                Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est. Donec efficitur nibh id ullamcorper molestie.
            </div>
-           <div class="poduct-1 product-grid">
+           <div class="product-1 product-grid">
                <div class="title">Product 1: Get Started</div>
                <div class="description">
                    Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est.
                </div>
            </div>
-           <div class="poduct-2 product-grid">
+           <div class="product-2 product-grid">
                <div class="title">Product 2: Advanced</div>
                <div class="description">
                    Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est.
                </div>
            </div>
-           <div class="poduct-3 product-grid">
+           <div class="product-3 product-grid">
                <div class="title">Product 3: Ultimate</div>
                <div class="description">
                    Suspendisse potenti. Praesent ac neque tempus, accumsan orci sed, laoreet magna. Aliquam mattis lorem ut mi ornare, non pretium nulla aliquet. Pellentesque at ex id leo accumsan pharetra gravida quis est.
@@ -61,11 +61,9 @@
        <div class="blog-feed-grid">
            @foreach($articles as $article)
                <div class="post-grid">
-                   <a href="/blog/{{ $article->id }}"><img src="{{ $article->cover->link }}"/></a>
-                   <div class="title"><a href="/blog/{{ $article->id }}">{{ $article->title }}</a></div>
-                   <div class="summary">
-                       {{ $article->summary }}<br/>
-                       <a href="/blog/{{ $article->id }}">Read More</a>
+                   <img src="{{ $article->cover->link }}"/>
+                   <div class="overlay">
+                        <div class="title"><a href="/blog/{{ $article->id }}">{{ $article->title }}</a></div>
                    </div>
                </div>
            @endforeach
